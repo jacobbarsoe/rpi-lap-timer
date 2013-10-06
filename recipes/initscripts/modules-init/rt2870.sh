cb7c37d2ad18ceb848d94817695a6ef977d87413
Jacob Kjaergaard <jacob.kjaergaard@prevas.dk>
2013-10-06 18:46:21 +0200

    fix wlan support on boot

---
+++ recipes/initscripts/modules-init/rt2870.sh
@@ -0,0 +1,8 @@
+#the following sequence avoids errors in dmesg
+modprobe crc-ccitt
+modprobe cfg80211
+modprobe mac80211
+modprobe rt2x00lib
+modprobe rt2800lib
+modprobe rt2x00usb
+modprobe rt2800usb
